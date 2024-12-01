24059435eced26dbf9f8d71788fc5043d783c0e1
Your Nameramadhanirani731@gmail.com <you@example.com>
2024-12-01 22:09:34 +0700

    Navbar dan Tabel

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Ini Adalah Halaman Mahasiswa</h1>
+    
+
+    <div class="row">
+        <div class="col-6">
+            <div class="container" style="margin-left: 20px">
+                <h4>Tabel Mahasiswa</h4>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nim</th>
+                    <th>Nama Mahasiswa</th>
+                </tr>
+            </thead>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
