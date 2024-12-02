fb54c2d512348ee6f17a47e4e2ce72a7909d8a5e
Your Nameramadhanirani731@gmail.com <you@example.com>
2024-12-02 12:49:28 +0700

    penerapan row dan col

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -5,24 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mahasiswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        table, th, td {
+            border: 1px solid black; /* Garis tabel sederhana */
+            border-collapse: collapse; /* Gabungkan garis antar sel */
+        }
+        th, td {
+            padding: 8px; /* Jarak di dalam sel */
+            text-align: center; /* Pusatkan teks */
+        }
+        
+    </style>
 </head>
 <body>
-    <h1>Ini Adalah Halaman Mahasiswa</h1>
-    
+   <h1> Halaman Mahasiswa </h1>
 
+   <div class="container" style="margin-left: 30px">
+   <table class="table"> 
     <div class="row">
         <div class="col-6">
-            <div class="container" style="margin-left: 20px">
-                <h4>Tabel Mahasiswa</h4>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nim</th>
-                    <th>Nama Mahasiswa</th>
-                </tr>
-            </thead>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <h4>Tabel Mahasiswa</h4>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nim</th>
+                <th>Nama</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < $jumlah; $i++)
+            <tr>
+                <td>{{$i+1}}</td>
+                <td>{{$nim[$i] }}</td>
+            </tr>
+                
+            @endfor
+        </tbody>
+        </div>
+    </div>
+   </table>
+   </div>
+    
 </body>
+<link rel="<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
